f816a41dd41053aab7370880fb1d67f861f1a19e
FGFW <yishanju@gmail.com>
2015-10-28 11:35:09 +0800

    (￣ˇ￣)

--- 判断C盘是否是SSD/批处理判断C盘是否是SSD.bat
+++ 判断C盘是否是SSD/批处理判断C盘是否是SSD.bat
@@ -1,7 +1,12 @@
-#批处理判断C盘是否是SSD.bat
-#依赖开源工具smartmontool中的ssmartctl.exe
+@echo off
 
-smartctl -i %systemdrive%|find "Solid State Device"&&echo 看来是SSD
+::批处理判断C盘是否是SSD.bat
+::依赖开源工具smartmontool中的ssmartctl.exe
+::http://sourceforge.net/projects/smartmontools/
+smartctl -i %systemdrive%|find "Solid State Device"&&echo 看来是SSD||echo - -不是SSD
 
-smartctl -a d: |find "Solid State Device"&&echo 看来是SSD
+smartctl -a d: |find "Solid State Device"&&echo 看来是SSD||echo - -不是SSD
 
+smartctl -a e: |find "Solid State Device"&&echo 看来是SSD||echo - -不是SSD
+
+pause
