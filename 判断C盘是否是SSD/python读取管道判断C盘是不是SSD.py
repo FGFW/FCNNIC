c79d7a6634a0aece032b426078751cf923fb7899
FGFW <yishanju@gmail.com>
2015-10-28 19:23:12 +0800

    (￣ˇ￣)

--- 判断C盘是否是SSD/python读取管道判断C盘是不是SSD.py
+++ 判断C盘是否是SSD/python读取管道判断C盘是不是SSD.py
@@ -1,12 +1,16 @@
 #python读取管道判断C盘是不是SSD
+#依赖开源工具smartmontool中的ssmartctl.exe
+#http://sourceforge.net/projects/smartmontools/
 
+#方法1
 import os
 txt=os.popen('smartctl -i e:').read()
 if 'Solid State Device' in txt:
     print("是SSD\n")
 else:
     print("也许大概不是SSD\n")
-    
+
+#方法2    
 #少数情况下C盘不是系统盘，从系统系统变量从读取系统盘符可靠性会高一些。
 cmd='smartctl -i '
 sd=os.environ.get('SYSTEMDRIVE')
