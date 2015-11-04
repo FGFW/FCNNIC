654526a813ef4d34c50c879e0c9f6448eb8b4579
FGFW <yishanju@gmail.com>
2015-11-05 05:23:10 +0800

    o(*≧▽≦)ツ

--- 实现一行内容分行输出/1.py
+++ 实现一行内容分行输出/1.py
@@ -1,3 +1,15 @@
+#一行内容分行输出
+#依山居 18:14 2015/11/4
+a="aA1一bB2二cC3三dD4四eE5五fF6六gG7七hH8八iI9九"
+for r in range(0,4):
+    t=''
+    for s in range(0+r,len(a),4):
+        t=t+a[s]
+    print(t)
 
-a=open("a.txt","r").readlines()
-txt=a[1]
+l=list(a)
+for r in range(0,4):
+    t=''
+    for s in range(0,9):
+        t=t+l.pop()
+    print(t)
