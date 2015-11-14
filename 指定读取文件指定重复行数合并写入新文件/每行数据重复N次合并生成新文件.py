ccd249d2139ef12c6f1f15aac7ae003d6dd4ceae
FGFW <yishanju@gmail.com>
2015-11-14 13:02:50 +0800

    o(*≧▽≦)ツ

--- 指定读取文件指定重复行数合并写入新文件/每行数据重复N次合并生成新文件.py
+++ 指定读取文件指定重复行数合并写入新文件/每行数据重复N次合并生成新文件.py
@@ -1,8 +1,12 @@
 ﻿"""
 每行数据重复N次合并生成新文件
+题目来源 http://www.bathome.net/thread-38017-2-1.html
 依山居 0:54 2015/11/14
-太困了,回头再改，6百万行，大约17秒。。。
-非常流氓地使用了sorted排序数据。实际数据中估计不能使用.
+这个版本可以使用来处理实际数据。。。6百万行，大约17秒。。。
+总结：几百万行数据真不算多。不需要逐行读取处理。python列表解析是个好东西~
+
+使用重复列表中元素更好的方法 http://www.oschina.net/question/96078_2141454
+python笔记_列表解析 http://www.jianshu.com/p/c635d3c798c2
 """
 
 import time
@@ -12,31 +16,32 @@ an=6
 with open("aa.txt") as f:
     ta=f.read()
     ta=ta.rsplit()
-    al=[(r+",") for r in ta]*an
-    al=sorted(al)
-print(len(al))   
+    al=[r+"," for r in ta for i in range(an)]
+print("al长度:",len(al))
+
 bn=3
 with open("bb.txt") as f:
     tb=f.read()
     tb=tb.rsplit()
-    bl=[(r+",") for r in tb]*bn
-    bl=sorted(bl)
-    
-print(len(bl))    
+    bl=[r+"," for r in tb for i in range(bn)]
+print("bl长度:",len(bl))
+
 cn=1
 with open("cc.txt") as f:
     tc=f.read()
     tc=tc.rsplit()
-    cl=[r+"\n" for r in tc]
-print(len(cl))
+    cl=[r+"\n" for r in tc for i in range(cn)]
+print("cl长度:",len(cl))
+
 end=time.time()
 pt=end-start
 print("运行耗时：",pt)
 
 rn=len(cl)
-tal=[]
-for r in range(rn):
-    tal.append(al[r]+bl[r]+cl[r])
+tal=[al[r]+bl[r]+cl[r] for r in range(rn)]
+#还是用列表解析好~
+#for r in range(rn):
+#   tal.append(al[r]+bl[r]+cl[r])
     
 end=time.time()
 pt=end-start
