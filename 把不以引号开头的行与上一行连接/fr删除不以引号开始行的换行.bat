b997638180ace6ba395f237aa820dc8113545311
FGFW <yishanju@gmail.com>
2015-11-19 16:54:07 +0800

    o(*≧▽≦)ツ 参考了网上的资料，python 读取文件默认使用通用模式，不管\r\n都会被转换成\n 所以正则可以简化(\n)，预计可以小幅提高处理速度

--- 把不以引号开头的行与上一行连接/fr删除不以引号开始行的换行.bat
+++ 把不以引号开头的行与上一行连接/fr删除不以引号开始行的换行.bat
@@ -1,11 +1,11 @@
-@echo off
+﻿@echo off
 ::依山居 13:01 2015/11/18
 ::fr 不带-stdout参数会直接修改原文件，所以复制s.txt为ss.txt运行测试
 ::fr 使用-s 可以列举子目录修改匹配的文件。注意做好备份
 copy a.txt ss.txt /y
 ::以引号结尾，不以引号开头，就删掉换行。这个规则好处理测试可行。
 
-fr ss.txt -rnnl:\"?(\r?\n+)[^\"] -t:
-::type ss.txt
+fr ss.txt -rnnl:\"?(\r\n)[^\"] -t:
+type ss.txt
 
 pause
--- 把不以引号开头的行与上一行连接/python处理db2del文件.py
+++ 把不以引号开头的行与上一行连接/python处理db2del文件.py
@@ -2,13 +2,15 @@
 python正则处理DEL文件换行问题
 http://www.bathome.net/thread-38164-1-1.html
 依山居 20:38 2015/11/18
+参考了网上的资料，python 读取文件默认使用通用模式，不管\r\n都会被转换成\n
+所以正则可以简化(\n)，预计可以小幅提高处理速度
 """
 import re
 import time
 start=time.time()
 
 of="a.txt"
-rec=re.compile('(\"?)(\r|\n|\r\n)([^\"])')
+rec=re.compile('(\"?)(\n)([^\"])')
 with open(of) as f:
     txt=f.read()
     res=re.sub(rec,r'\1\3',txt)
