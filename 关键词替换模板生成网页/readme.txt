fa469d8a78e0468a50260d655859d9ff375f36c4
FGFW <yishanju@gmail.com>
2015-11-24 01:53:38 +0800

    o(*≧▽≦)ツ 关键词替换模板生成网页 鄙视做垃圾站的~

--- 关键词替换模板生成网页/readme.txt
+++ 关键词替换模板生成网页/readme.txt
@@ -1,4 +1,4 @@
-
+http://www.bathome.net/thread-37777-1-1.html
 1，读入域名文本host.txt，然后按顺序生成，生成域名文件夹/二级文件夹www.域名/生成index.htm
 2，index.htm模板从moban.htm提取（moban.htm设置一个标签为{关键词1}{关键词2}{关键词3}{关键词4}）
 3，关键字标签随机读取key.txt
---
+++ 关键词替换模板生成网页/关键词替换模板生成网页.py
@@ -0,0 +1,26 @@
+﻿"""
+http://www.bathome.net/thread-37777-1-1.html
+依山居 1:07 2015/11/24
+→_→强烈鄙视做垃圾站的
+"""
+import random
+import os
+
+with open("host.txt",encoding="utf-8") as f:
+    host=[r.rstrip() for r in f.readlines()]
+with open("key.txt",encoding="utf-8") as f:
+    key=[r.rstrip() for r in f.readlines()]
+with open("moban.txt",encoding="utf-8") as f:
+    moban=f.read()
+for h in host:
+    rkey=random.sample(key,4)
+    dirs="./"+h+"/"+"www."+h+"/"
+    html=moban
+    #一行代码长到天涯海角~
+    html=html.replace("{关键词1}",rkey[0]).replace("{关键词2}",rkey[1]).replace("{关键词3}",rkey[2]).replace("{关键词4}",rkey[3])
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+    with open(dirs+"index.htm","w+",encoding="utf-8") as f:
+        f.write(html)
+
+
