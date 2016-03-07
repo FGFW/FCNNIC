5f27a4770ecede97502f07f4d27db09e3489864c
FGFW <yishanju@gmail.com>
2016-03-08 07:43:27 +0800

    python解无忧公主数学题108回文.py

--- python解无忧公主数学题/python算无忧公主数学题108回文.py
+++ python解无忧公主数学题/python算无忧公主数学题108回文.py
@@ -7,11 +7,11 @@ python解无忧公主数学题108回文.py
 #利用到了集合的特性，进行集合交集运算
 #2016年3月8日 05:59:41
 def 方法1():
-    f=lambda x,y: x if str(x*y)==str(x*y)[::-1] else None
-    x91={f(r,91) for r in range(1,100) if f(r,91)}
-    x93={f(r,93) for r in range(1,100) if f(r,93)}
-    x95={f(r,95) for r in range(1,100) if f(r,95)}
-    x97={f(r,97) for r in range(1,100) if f(r,97)}
+    f=lambda x,y: True if str(x*y)==str(x*y)[::-1] else None
+    x91={r for r in range(1,100) if f(r,91)}
+    x93={r for r in range(1,100) if f(r,93)}
+    x95={r for r in range(1,100) if f(r,95)}
+    x97={r for r in range(1,100) if f(r,97)}
     print("方法1结果:",x91&x93&x95&x97)
     return (x91&x93&x95&x97)
 
@@ -31,3 +31,16 @@ def ff2():
 
 ff2()
         
+#2016年3月8日 06:59:08
+#用正则匹配写一个
+import re
+def ff3():
+    def f(x,y):
+        if re.match(r"""^(\d?)(\d?)(\d?)(\d?)\4\3\2\1$""",str(x*y)):
+            return True
+    x91=[r for r in range(1,100) if f(r,91)]
+    results=[r for r in x91 if f(r,93) and f(r,95) and f(r,97)]
+    print("方法3结果:",results)
+    #2016年3月8日 07:29:33
+
+ff3()
