2ab577dc1d89f5e370bd3cd15c9a2063e38a746c
FGFW <yishanju@gmail.com>
2016-03-20 20:38:20 +0800

    wakeonlan python

--- littlescript/python wakeonlan实现电脑网络开机.py
+++ littlescript/python wakeonlan实现电脑网络开机.py
@@ -8,16 +8,14 @@ https://pypi.python.org/pypi/wakeonlan/0.2.2
 pip命令安装模块
 pip install wakeonlan
 
-把mac地址收集后,一行一个存在mac.txt中
-
+把mac地址收集后,一行一个存在mac.txt中.
+使用Wireshark抓了下包,发现多网卡的环境下是使用其中的一个网卡发送的.
+广播包可能发不到指定的网段里,所以需要指定一下目标网段的IP.
 """
 from wakeonlan import wol
 
 with open("mac.txt") as f:
     for mac in f:
-        wol.send_magic_packet(mac.strip())
+        wol.send_magic_packet(mac.strip(),ip_address="192.168.255.255")
 
-        
-        
-        
-    
+
