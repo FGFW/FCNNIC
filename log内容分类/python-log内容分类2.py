204dc34e015f41bd8fbf73089f2d4138c84f0732
FGFW <yishanju@gmail.com>
2016-03-24 03:07:45 +0800

    ython-log内容分类2.py

--- log内容分类/python-log内容分类2.py
+++ log内容分类/python-log内容分类2.py
@@ -6,8 +6,8 @@ http://stackoverflow.com/questions/36182906/regex-matching-of-numbers-and-redire
 with open("event_1.log") as f,open("0.log","w+") as f0, open("1.log","w+") as f1:
     for line in f:
         sl=line.split()
-        result0=sl[1]=="0" and sl[0]=="a" or sl[1]=="d"
-        result1=sl[1]=="1" and sl[0]=="a" or sl[1]=="d"
+        result0=sl[1]=="0" and (sl[0]=="a" or sl[0]=="d")
+        result1=sl[1]=="1" and (sl[0]=="a" or sl[0]=="d")
         if result0:
             f0.write(line)
             print(line)
